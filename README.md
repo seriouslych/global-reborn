7abee2600dcf2e55bb438ddd96cdf72f9f14e105
Alexander Seriously <120324465+seriouslych@users.noreply.github.com>
2024-08-28 12:26:23 +0300

    GLOBAL-REBORN REBRANDING

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# GC-REBORN
+# GLOBAL-REBORN
 
 ### EN:
 
-GC-REBORN - is a Discord bot that send messages, files and gifs all around different servers that have this bot.
+**GLOBAL-REBORN** - is a Discord bot that send messages, files and gifs all around different servers that have this bot.
 
 Made by [seriouslych](https://github.com/seriouslych)
 
 ### RU:
 
-GC-REBORN - это Discord бот который отправляет сообщения, файлы и гиф на разные серверы, у которых есть этот бот.
+**GLOBAL-REBORN** - это Discord бот который отправляет сообщения, файлы и гиф на разные серверы, у которых есть этот бот.
 
 Сделано [seriouslych](https://github.com/seriouslych)
--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 import database
 
 """
-============= GC-REBORN ==============
+============= GLOBAL-REBORN ==============
 EN:
-GC-REBORN - is a Discord bot that send messages, files and gifs all around different servers that have this bot.
+GLOBAL-REBORN - is a Discord bot that send messages, files and gifs all around different servers that have this bot.
 Made by seriouslych (https://github.com/seriouslych)
 
 RU:
-GC-REBORN - это Discord бот который отправляет сообщения, файлы и гиф на разные серверы, у которых есть этот бот.
+GLOBAL-REBORN - это Discord бот который отправляет сообщения, файлы и гиф на разные серверы, у которых есть этот бот.
 Сделано seriouslych (https://github.com/seriouslych)
 ======================================
 """
@@ -21,6 +21,7 @@ TODO:
 - Систему администрирования (мьюты и баны)
 - Систему логирования
 - Команду help
+- Определение других хостингов кроме Tenor
 """
 
 # загрузка токена с .env файла
